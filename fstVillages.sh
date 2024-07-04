4c04bdaf1bdd08900f96557211097f0863439013
Tushabe Vianney <tushabejohnvianney@YAHOO.COM>
2024-07-04 09:32:59 +0300

    Add files via upload

---
+++ fstVillages.sh
@@ -0,0 +1,83 @@
+bsub.py --threads 3 -q normal 4 Kakeeka_Kachanga ~jc17/software/vcftools_0.1.15/vcftools --vcf Bi_174_new.recode.vcf \
+        --weir-fst-pop ../../../Sample.lists/Kakeeka.txt --weir-fst-pop ../../../Sample.lists/Kachanga.txt --out Kakeeka_Kachanga
+
+bsub.py --threads 3 -q normal 4 Zingola_Katooke ~jc17/software/vcftools_0.1.15/vcftools --vcf Bi_174_new.recode.vcf \
+        --weir-fst-pop ../../../Sample.lists/Zingoola.txt --weir-fst-pop ../../../Sample.lists/Katooke.txt --out Zingoola_Katooke
+
+bsub.py --threads 3 -q normal 4 Zingola_Kachanga ~jc17/software/vcftools_0.1.15/vcftools --vcf Bi_174_new.recode.vcf \
+        --weir-fst-pop ../../../Sample.lists/Zingoola.txt --weir-fst-pop ../../../Sample.lists/Kachanga.txt --out Zingoola_Kachanga
+
+bsub.py --threads 3 -q normal 4 Katooke_Kachanga ~jc17/software/vcftools_0.1.15/vcftools --vcf Bi_174_new.recode.vcf \
+        --weir-fst-pop ../../../Sample.lists/Katooke.txt --weir-fst-pop ../../../Sample.lists/Kachanga.txt --out Katooke_Kachanga
+
+bsub.py --threads 3 -q normal 4 Zingoola_Busi ~jc17/software/vcftools_0.1.15/vcftools --vcf Bi_174_new.recode.vcf \
+        --weir-fst-pop ../../../Sample.lists/Busi.txt --weir-fst-pop ../../../Sample.lists/Zingoola.txt --out Zingoola_Busi
+
+bsub.py --threads 3 -q normal 4 Kachanga_Busi ~jc17/software/vcftools_0.1.15/vcftools --vcf Bi_174_new.recode.vcf \
+        --weir-fst-pop ../../../Sample.lists/Busi.txt --weir-fst-pop ../../../Sample.lists/Kachanga.txt --out Kachanga_Busi
+
+bsub.py --threads 3 -q normal 4 Kachanga_Kisu ~jc17/software/vcftools_0.1.15/vcftools --vcf Bi_174_new.recode.vcf \
+        --weir-fst-pop ../../../Sample.lists/Kisu.txt --weir-fst-pop ../../../Sample.lists/Kachanga.txt --out Kachanga_Kisu
+
+bsub.py --threads 3 -q normal 4 Katooke_Busi ~jc17/software/vcftools_0.1.15/vcftools --vcf Bi_174_new.recode.vcf \
+        --weir-fst-pop ../../../Sample.lists/Busi.txt --weir-fst-pop ../../../Sample.lists/Katooke.txt --out Katooke_Busi
+
+bsub.py --threads 3 -q normal 4 Katooke_Kisu ~jc17/software/vcftools_0.1.15/vcftools --vcf Bi_174_new.recode.vcf \
+        --weir-fst-pop ../../../Sample.lists/Kisu.txt --weir-fst-pop ../../../Sample.lists/Katooke.txt --out Katooke_Kisu
+
+bsub.py --threads 3 -q normal 4 Zingoola_Kisu ~jc17/software/vcftools_0.1.15/vcftools --vcf Bi_174_new.recode.vcf \
+        --weir-fst-pop ../../../Sample.lists/Kisu.txt --weir-fst-pop ../../../Sample.lists/Zingoola.txt --out Zingoola_Kisu
+
+bsub.py --threads 3 -q normal 4 Zingola_Kakeeka ~jc17/software/vcftools_0.1.15/vcftools --vcf Bi_174_new.recode.vcf \
+        --weir-fst-pop ../../../Sample.lists/Kakeeka.txt --weir-fst-pop ../../../Sample.lists/Zingoola.txt --out Zingoola_Kakeeka
+
+bsub.py --threads 3 -q normal 4 Katooke_Lugumba ~jc17/software/vcftools_0.1.15/vcftools --vcf Bi_174_new.recode.vcf \
+        --weir-fst-pop ../../../Sample.lists/Katooke.txt --weir-fst-pop ../../../Sample.lists/Lugumba.txt --out Katooke_Lugumba
+
+bsub.py --threads 3 -q normal 4 zingoola_kitosi ~jc17/software/vcftools_0.1.15/vcftools --vcf Bi_174_new.recode.vcf \
+        --weir-fst-pop ../../../Sample.lists/Zingoola.txt --weir-fst-pop ../../../Sample.lists/Kitosi.txt --out Kitosi_Zingoola
+
+bsub.py --threads 3 -q normal 4 zingola_lugumba ~jc17/software/vcftools_0.1.15/vcftools --vcf Bi_174_new.recode.vcf \
+        --weir-fst-pop ../../../Sample.lists/Zingoola.txt --weir-fst-pop ../../../Sample.lists/Lugumba.txt --out Lugumba_Zingoola
+
+bsub.py --threads 3 -q normal 4 kakeeka_katooke ~jc17/software/vcftools_0.1.15/vcftools --vcf Bi_174_new.recode.vcf \
+        --weir-fst-pop ../../../Sample.lists/Kakeeka.txt --weir-fst-pop ../../../Sample.lists/Katooke.txt --out Kakeeka_Katooke
+
+bsub.py --threads 3 -q normal 4 kitosi_katooke ~jc17/software/vcftools_0.1.15/vcftools --vcf Bi_174_new.recode.vcf \
+        --weir-fst-pop ../../../Sample.lists/Kitosi.txt --weir-fst-pop ../../../Sample.lists/Katooke.txt --out Kitosi_Katooke
+
+bsub.py --threads 3 -q normal 4 busi_kakeeka ~jc17/software/vcftools_0.1.15/vcftools --vcf Bi_174_new.recode.vcf \
+        --weir-fst-pop ../../../Sample.lists/Busi.txt --weir-fst-pop ../../../Sample.lists/Kakeeka.txt --out Kakeeka_Busi
+
+bsub.py --threads 3 -q normal 4 kisu_kakeeka ~jc17/software/vcftools_0.1.15/vcftools --vcf Bi_174_new.recode.vcf \
+        --weir-fst-pop ../../../Sample.lists/Kisu.txt --weir-fst-pop ../../../Sample.lists/Kakeeka.txt --out Kakeeka_Kisu
+
+bsub.py --threads 3 -q normal 4 lugumba_busi ~jc17/software/vcftools_0.1.15/vcftools --vcf Bi_174_new.recode.vcf \
+        --weir-fst-pop ../../../Sample.lists/Lugumba.txt --weir-fst-pop ../../../Sample.lists/Busi.txt --out Busi_Lugumba
+
+bsub.py --threads 3 -q normal 4 kisu_busi ~jc17/software/vcftools_0.1.15/vcftools --vcf Bi_174_new.recode.vcf \
+        --weir-fst-pop ../../../Sample.lists/Kisu.txt --weir-fst-pop ../../../Sample.lists/Busi.txt --out Busi_Kisu
+
+bsub.py --threads 3 -q normal 4 kitosi_kachanga ~jc17/software/vcftools_0.1.15/vcftools --vcf Bi_174_new.recode.vcf \
+        --weir-fst-pop ../../../Sample.lists/Kitosi.txt --weir-fst-pop ../../../Sample.lists/Kachanga.txt --out Kitosi_Kachanga
+
+bsub.py --threads 3 -q normal 4 kitosi_busi ~jc17/software/vcftools_0.1.15/vcftools --vcf Bi_174_new.recode.vcf \
+        --weir-fst-pop ../../../Sample.lists/Kitosi.txt --weir-fst-pop ../../../Sample.lists/Busi.txt --out Kitosi_Busi
+
+bsub.py --threads 3 -q normal 4 kitosi_lugumba ~jc17/software/vcftools_0.1.15/vcftools --vcf Bi_174_new.recode.vcf \
+        --weir-fst-pop ../../../Sample.lists/Kitosi.txt --weir-fst-pop ../../../Sample.lists/Lugumba.txt --out Kitosi_Lugumba
+
+bsub.py --threads 3 -q normal 4 kachanga_lugumba ~jc17/software/vcftools_0.1.15/vcftools --vcf Bi_174_new.recode.vcf \
+        --weir-fst-pop ../../../Sample.lists/Kachanga.txt --weir-fst-pop ../../../Sample.lists/Lugumba.txt --out Kachanga_Lugumba
+
+bsub.py --threads 3 -q normal 4 kitosi_kisu ~jc17/software/vcftools_0.1.15/vcftools --vcf Bi_174_new.recode.vcf \
+        --weir-fst-pop ../../../Sample.lists/Kitosi.txt --weir-fst-pop ../../../Sample.lists/Kisu.txt --out Kitosi_Kisu
+
+bsub.py --threads 3 -q normal 4 lugumba_kisu ~jc17/software/vcftools_0.1.15/vcftools --vcf Bi_174_new.recode.vcf \
+        --weir-fst-pop ../../../Sample.lists/Lugumba.txt --weir-fst-pop ../../../Sample.lists/Kisu.txt --out Lugumba_Kisu
+
+bsub.py --threads 3 -q normal 4 Kakeeka_kitosi ~jc17/software/vcftools_0.1.15/vcftools --vcf Bi_174_new.recode.vcf \
+        --weir-fst-pop ../../../Sample.lists/Kakeeka.txt --weir-fst-pop ../../../Sample.lists/Kitosi.txt --out Kakeeka_Kitosi
+
+bsub.py --threads 3 -q normal 4 Kakeeka_lugumba ~jc17/software/vcftools_0.1.15/vcftools --vcf Bi_174_new.recode.vcf \
+        --weir-fst-pop ../../../Sample.lists/Kakeeka.txt --weir-fst-pop ../../../Sample.lists/Lugumba.txt --out Kakeeka_Lugumba
